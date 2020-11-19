d23948b30629fe7c119830c4c8b7743188c3a90a
Mylan DEVEAU <mylan.deveau@etu.unilim.fr>
2020-11-19 17:11:01 +0100

    console

--- src/main/java/RPG.java
+++ src/main/java/RPG.java
@@ -2,22 +2,42 @@ package main.java;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import main.java.controllers.ConsoleController;
+import main.java.controllers.RPGController;
 
 public class RPG extends Application {
 
+    BorderPane rootLayout;
+    ConsoleController console;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/RPG.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("views/RPG.fxml"));
+        rootLayout = loader.load();
+        RPGController controller = loader.getController();
+        controller.setParent(this);
+        loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("views/Console.fxml"));
+        TextFlow tf = loader.load();
+        rootLayout.setLeft(tf);
+        console = loader.getController();
+
         primaryStage.setTitle("RPG");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(rootLayout, 1000, 1100));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void afficheConsole(String text) {
+        console.afficher(text);
     }
 }
